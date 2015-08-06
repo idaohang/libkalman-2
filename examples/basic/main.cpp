65588374014804701443b7f89310a7a85f58e9b3
Vincent Zalzal <vincent.zalzal@gmail.com>
2015-08-06 00:14:27 -0400

    Added copyright notices at start of files (doxygen format). Added the main ekf header.

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -1,6 +1,18 @@
+/// \file
+/// \author Vincent Zalzal
+/// \date   2015
+/// \copyright The MIT license (see LICENSE file)
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
 #include <libkalman/extended_kalman.h>
+
+namespace ublas = boost::numeric::ublas;
 
 int main()
 {
+    ublas::vector<double> v(3);
+    for (unsigned i = 0; i < v.size(); ++i)
+        v(i) = i;
     return 0;
 }
---
+++ include/libkalman/extended_kalman.h
@@ -0,0 +1,5 @@
+/// \file
+/// \author Vincent Zalzal
+/// \date   2015
+/// \copyright The MIT license (see LICENSE file)
+#pragma once
